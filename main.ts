672fecc808eba9916d42a4eafd31972618c322bc
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-03-07 15:28:15 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% block="Color Sensor" block.loc.cs="Senzor Barev" color=#00B1ED  icon=""
+//% block="Color Sensor" block.loc.cs="Senzor Barev" color=#00B1ED  icon="\U{palette}"
 namespace ColorSensor {
     const APDS9960_ADDR = 0x39
     const APDS9960_ENABLE = 0x80
