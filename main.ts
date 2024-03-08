55c72b65feb94d03f2f92a5767deb10c0777a0ad
Pythom1234 <pythom1234@email.cz>
2024-03-08 14:12:21 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -3,17 +3,20 @@ namespace ColorSensor {
     const ADDR = 0x39;
     let first_init = false
     let tmp
+    //% block
     export function i2cwrite(addr: number, reg: number, value: number) {
         let buf = pins.createBuffer(2)
         buf[0] = reg
         buf[1] = value
         pins.i2cWriteBuffer(addr, buf)
     }
+    //% block
     export function i2cread(addr: number, reg: number) {
         pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
         let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
         return val;
     }
+    //% block
     export function initModule(): void {
         i2cwrite(ADDR, 0x81, 0b11111100)
         i2cwrite(ADDR, 0x8F, 0b1111)
@@ -23,6 +26,7 @@ namespace ColorSensor {
         i2cwrite(ADDR, 0x80, 0b1000111)
         first_init = true
     }
+    //% block
     export function calibration(): void {
         tmp = i2cread(ADDR, 0x93) & 0x1;
         while (!tmp) {
@@ -34,7 +38,7 @@ namespace ColorSensor {
     //% block.loc.cs="vzdálenost (0~255)"
     //% weight=100
     export function distance(): number {
-        //calibration()
+        calibration()
         return i2cread(ADDR, 0x9C)
     }
 }
