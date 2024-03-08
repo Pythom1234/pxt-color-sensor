69ef84c2758d389cc59a8f0ce6734a1d80a59b9c
Pythom1234 <pythom1234@email.cz>
2024-03-08 14:47:41 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -24,7 +24,8 @@ namespace ColorSensor {
         i2cwrite(ADDR, 0x80, 0b0)
         i2cwrite(ADDR, 0xAB, 0b0)
         i2cwrite(ADDR, 0xE7, 0b0)
-        i2cwrite(ADDR, 0x80, 0b1000111)
+        //i2cwrite(ADDR, 0x80, 0b1000111)
+        i2cwrite(ADDR, 0x80, 0b1)
         first_init = true
     }
     //% block
