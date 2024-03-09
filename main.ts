c497acbfdebec667ca515dda141dbd07368991cc
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-03-09 09:43:40 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
 //% block="Color Sensor" block.loc.cs="Senzor Barev" color=#00B1ED  icon="\uf005"
 namespace ColorSensor {
+    export enum Mode {
+        //% block="distance" block.loc.cs="vzdálenost"
+        Distance,
+        //% block="color" block.loc.cs="barvy"
+        Color,
+        //% block="gesture" block.loc.cs="gesta"
+        Gesture
+    }
     const ADDR = 0x39
     let tmp
+    let currentMode: Mode
     export function i2cwrite(addr: number, reg: number, value: number) {
         let buf = pins.createBuffer(2)
         buf[0] = reg
@@ -13,29 +22,49 @@ namespace ColorSensor {
         let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE)
         return val
     }
-    //% block="initalize"
-    //% block.loc.cs="inicializovat"
+    //% block="initalize mode $mode"
+    //% block.loc.cs="inicializovat mód $mode"
     //% weight=100
-    export function init(): void {
-        i2cwrite(ADDR, 0x80, 0b0)
-        i2cwrite(ADDR, 0x81, 0b11111100)
-        i2cwrite(ADDR, 0x8F, 0b11001111)
-        i2cwrite(ADDR, 0xAB, 0b0)
-        i2cwrite(ADDR, 0xE7, 0b0)
-        i2cwrite(ADDR, 0x80, 0b1110001)
+    export function init(mode: Mode): void {
+        currentMode = mode
+        switch (mode) {
+            case Mode.Distance:
+                i2cwrite(ADDR, 0x80, 0b0)
+                i2cwrite(ADDR, 0x81, 0b11111100)
+                i2cwrite(ADDR, 0x8F, 0b11001111)
+                i2cwrite(ADDR, 0xAB, 0b0)
+                i2cwrite(ADDR, 0xE7, 0b0)
+                i2cwrite(ADDR, 0x80, 0b101)
+            case Mode.Color:
+                i2cwrite(ADDR, 0x80, 0b0)
+                i2cwrite(ADDR, 0x81, 0b11111100)
+                i2cwrite(ADDR, 0x8F, 0b11001111)
+                i2cwrite(ADDR, 0xAB, 0b0)
+                i2cwrite(ADDR, 0xE7, 0b0)
+                i2cwrite(ADDR, 0x80, 0b11)
+            case Mode.Gesture:
+                i2cwrite(ADDR, 0x80, 0b0)
+                i2cwrite(ADDR, 0x81, 0b11111100)
+                i2cwrite(ADDR, 0x8F, 0b11001111)
+                i2cwrite(ADDR, 0xAB, 0b0)
+                i2cwrite(ADDR, 0xE7, 0b0)
+                i2cwrite(ADDR, 0x80, 0b1000101)
+        }
     }
     //% block="distance (0~255)"
     //% block.loc.cs="vzdálenost (0~255)"
     //% weight=99
     export function distance(): number {
-        i2cwrite(ADDR, 0x80, 0b101)
-        tmp = i2cread(ADDR, 0x93) & 0b10;
-        while (!tmp) {
-            basic.pause(5);
+        if (currentMode == Mode.Distance) {
             tmp = i2cread(ADDR, 0x93) & 0b10;
+            while (!tmp) {
+                basic.pause(5);
+                tmp = i2cread(ADDR, 0x93) & 0b10;
+            }
+            return i2cread(ADDR, 0x9C)
+        } else {
+            throw "mode is not `distance`"
         }
-        return i2cread(ADDR, 0x9C)
-        i2cwrite(ADDR, 0x80, 0b1110001)
     }
 }
 
